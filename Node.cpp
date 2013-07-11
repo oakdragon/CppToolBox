6dbcb2f500949baa0ffcd778afefcc91ec56b4c1
oakdragon <braincpp@gmail.com>
2013-07-11 09:15:13 -0700

    Empty Node

---
+++ Node.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Node.cpp
+ * Author: brain
+ * 
+ * Created on July 11, 2013, 9:11 AM
+ */
+
+#include "Node.h"
+
+Node::Node() {
+}
+
+Node::Node(const Node& orig) {
+}
+
+Node::~Node() {
+}
+
---
+++ Node.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Node.h
+ * Author: brain
+ *
+ * Created on July 11, 2013, 9:11 AM
+ */
+
+#ifndef NODE_H
+#define	NODE_H
+
+class Node {
+public:
+    Node();
+    Node(const Node& orig);
+    virtual ~Node();
+private:
+
+};
+
+#endif	/* NODE_H */
+
